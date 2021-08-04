28f46a03964aca681e320a1271fc6c18db8eab78
kasmr <alikkras@gmail.com>
2021-08-05 01:07:31 +0300

    finished with images of russian ruble :)

--- state-exercises/src/CoinFlipper.jsx
+++ state-exercises/src/CoinFlipper.jsx
@@ -4,14 +4,54 @@ import Coins from './Coins';
 export default class CoinFlipper extends Component {
   constructor(props) {
     super(props);
-    this.state = { flipCount: 0, tailCount: 0, headCount: 0 };
+    this.state = { flipCount: 0, tailCount: 0, headCount: 0, coin: null };
   }
+
+  flipArrayVariants = ['tail', 'head'];
+
+  incrementFlipCount = (currentState) => {
+    return { flipCount: currentState.flipCount + 1 };
+  };
+
+  incrementHeadCount = (currentState) => {
+    return { headCount: currentState.headCount + 1 };
+  };
+
+  incrementTailCount = (currentState) => {
+    return { tailCount: currentState.tailCount + 1 };
+  };
+
+  flippingCoin = () => {
+    let flippedCoin =
+      this.flipArrayVariants[
+        Math.floor(Math.random() * this.flipArrayVariants.length)
+      ];
+    if (flippedCoin === 'tail') {
+      this.setState(this.incrementTailCount);
+      this.setState({ coin: 'tail' });
+    } else {
+      this.setState(this.incrementHeadCount);
+      this.setState({ coin: 'head' });
+    }
+  };
+
+  handleClick = () => {
+    this.setState(this.incrementFlipCount);
+    this.flippingCoin();
+  };
 
   render() {
     return (
-      <div>
-        <Coins />
-      </div>
+      <>
+        {!this.state.coin && <h1>Let's flip a coin my friend!</h1>}
+        <Coins flipCount={this.state.flipCount} coin={this.state.coin} />
+        <button onClick={this.handleClick}>Flip a coin!</button>
+        <p>
+          Out of <b>{this.state.flipCount}</b> there have been{' '}
+          <b>{this.state.tailCount}</b> tail and <b>{this.state.headCount}</b>{' '}
+          heads
+        </p>
+      </>
     );
   }
 }
--- state-exercises/src/Coins.jsx
+++ state-exercises/src/Coins.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import tail from './assets/tail.jpg';
+import head from './assets/head.jpg';
 
 export default class Coins extends Component {
   render() {
-    return (
-      <div>
-        <img src='' alt='head' />
-        <img src='' alt='tail' />
-      </div>
-    );
+    if (this.props.coin) {
+      return this.props.coin === 'tail' ? (
+        <img src={tail} alt={this.props.coin} />
+      ) : (
+        <img src={head} alt={this.props.coin} />
+      );
+    } else {
+      return null;
+    }
   }
 }
