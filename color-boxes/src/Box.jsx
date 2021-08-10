931626507b74497b08e6225cbc4b140a130c4e2e
kasmr <alikkras@gmail.com>
2021-08-10 22:27:10 +0300

    now working but issue with the same color after the change of initial color

--- color-boxes/src/Box.jsx
+++ color-boxes/src/Box.jsx
@@ -4,6 +4,7 @@ export default class Box extends Component {
   render() {
     return (
       <div
+        onClick={() => this.props.handleClick(this.props.box)}
         className='box'
         style={{ backgroundColor: this.props.color }}
         id={this.props.id}
--- color-boxes/src/BoxesContainer.jsx
+++ color-boxes/src/BoxesContainer.jsx
@@ -1,32 +1,51 @@
 import React, { Component } from 'react';
 import Box from './Box.jsx';
-import { v4 as uuidv4, v4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 
 export default class BoxesContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      newColor: null,
       boxes: this.generateArray(),
     };
   }
+
+  generateRandomColor = () =>
+    `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 
   generateArray = () => {
     let array = [];
     for (let index = 0; index < 18; index++) {
       array.push({
-        color: `#${Math.floor(Math.random() * 16777215).toString(16)}`,
-        id: v4(),
+        color: this.generateRandomColor(),
+        newColor: null,
+        id: uuidv4(),
       });
     }
     return array;
+  };
+
+  handleClick = (clickedBox) => {
+    const newBoxes = [...this.state.boxes];
+
+    newBoxes.map((box) =>
+      clickedBox.id === box.id ? (box.color = this.generateRandomColor()) : box
+    );
+
+    this.setState((state) => ({ newBoxes }));
   };
 
   render() {
     return (
       <div className='container'>
         {this.state.boxes.map((box) => (
-          <Box key={box.id} color={box.color} />
+          <Box
+            key={box.id}
+            color={box.color}
+            handleClick={this.handleClick}
+            id={box.id}
+            box={box}
+          />
         ))}
       </div>
     );
