95be94dc8c13101d3e21897844b3e2f500992021
kasmr <alikkras@gmail.com>
2021-08-31 22:06:36 +0300

    getting new jokes and changing the score + styling

--- dad-jokes/src/DadJokes.jsx
+++ dad-jokes/src/DadJokes.jsx
@@ -11,39 +11,71 @@ export default class DadJokes extends Component {
   }
 
   async componentDidMount() {
-    const getJokes = await fetch('https://icanhazdadjoke.com/search?limit=15', {
+    const getJokes = await fetch('https://icanhazdadjoke.com/search', {
       headers: { Accept: 'application/json' },
     });
 
     const { results, current_page } = await getJokes.json();
 
-    this.setState({ dadJokes: results, currPage: current_page });
+    const dadJokesArray = results.map((item) => ({
+      joke: item.joke,
+      id: item.id,
+      score: 5,
+    }));
+
+    this.setState({ dadJokes: dadJokesArray, currPage: current_page });
   }
 
   handleNewJokes = async () => {
     const getNewJokes = await fetch(
-      `https://icanhazdadjoke.com/search?limit=15?page=${this.state.currPage}`,
+      `https://icanhazdadjoke.com/search?page=${this.state.currPage + 1}`,
       {
         headers: { Accept: 'application/json' },
       }
     );
 
-    const { results, current_page } = await getNewJokes.json();
+    const { results } = await getNewJokes.json();
 
-    this.setState({ dadJokes: results, currPage: this.state.currPage + 1 });
+    const dadJokesArray = results.map((item) => ({
+      joke: item.joke,
+      id: item.id,
+      score: 5,
+    }));
+
+    this.setState((prevSt) => ({
+      dadJokes: dadJokesArray,
+      currPage: prevSt.currPage + 1,
+    }));
+  };
+
+  handleScoreChange = (id, delta) => {
+    this.setState((prevSt) => ({
+      dadJokes: prevSt.dadJokes.map((joke) =>
+        joke.id === id ? { ...joke, score: joke.score + delta } : joke
+      ),
+    }));
   };
 
   render() {
     return (
       <div className='dad-jokes'>
-        <div>
-          {' '}
-          <button onClick={this.handleNewJokes}>refresh</button>
+        <div className='refresh'>
+          <i className='fas fa-sync' onClick={this.handleNewJokes} />
         </div>
         <div className='dad-jokes list'>
-          {this.state.dadJokes.map((item) => (
-            <SingleJoke joke={item.joke} key={item.id} />
-          ))}
+          {this.state.dadJokes.length === 0 ? (
+            <h1>There is no any jokes left</h1>
+          ) : (
+            this.state.dadJokes.map((item) => (
+              <SingleJoke
+                joke={item.joke}
+                id={item.id}
+                score={item.score}
+                key={item.id}
+                handleScoreChange={this.handleScoreChange}
+              />
+            ))
+          )}
         </div>
       </div>
     );
--- dad-jokes/src/SingleJoke.jsx
+++ dad-jokes/src/SingleJoke.jsx
@@ -1,40 +1,32 @@
 import React, { Component } from 'react';
 
 export default class SingleJoke extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { counter: 5 };
-  }
-
-  handleUp = () => {
-    if (this.state.counter !== 10)
-      this.setState((prevSt) => ({ counter: prevSt.counter + 1 }));
-  };
-  handleDown = () => {
-    if (this.state.counter !== 0)
-      this.setState((prevSt) => ({ counter: prevSt.counter - 1 }));
-  };
-
   render() {
     let circleColor = 'orange';
     let emoji = 'far fa-meh';
 
-    if (this.state.counter <= 3) {
+    if (this.props.score <= 3) {
       circleColor = 'red';
       emoji = 'far fa-meh-rolling-eyes';
     }
-    if (this.state.counter >= 7) {
+    if (this.props.score >= 7) {
       circleColor = 'green';
       emoji = 'far fa-laugh-beam';
     }
     return (
       <div className='joke'>
         <div className='buttons'>
-          <i class='fas fa-arrow-up' onClick={this.handleUp}></i>
+          <i
+            className='fas fa-arrow-up'
+            onClick={() => this.props.handleScoreChange(this.props.id, +1)}
+          />
           <p style={{ border: `solid 3px ${circleColor}` }}>
-            {this.state.counter}
+            {this.props.score}
           </p>
-          <i class='fas fa-arrow-down' onClick={this.handleDown}></i>
+          <i
+            className='fas fa-arrow-down'
+            onClick={() => this.props.handleScoreChange(this.props.id, -1)}
+          />
         </div>
         <p>{this.props.joke}</p>
         <i className={emoji} />
